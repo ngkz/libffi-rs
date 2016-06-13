e9f9d05b49339fc36645ee0e9237c08283498b34
Jesse A. Tov <jesse.tov@gmail.com>
2016-06-13 07:51:34 -0600

    Update documentation

--- implementors/libffi/high/types/trait.CType.js
+++ implementors/libffi/high/types/trait.CType.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libffi'] = [];implementors['libffi'] = [];implementors['libffi'] = [];implementors['libffi'] = [];implementors['libffi'] = [];implementors['libffi'] = [];implementors['libffi'] = [];implementors['libffi'] = [];implementors['libffi'] = [];implementors['libffi'] = [];implementors['libffi'] = [];
+implementors['libffi'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
